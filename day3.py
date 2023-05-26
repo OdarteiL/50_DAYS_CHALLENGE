80b617b400294455c9fb8dcacd570da951ba6fca
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2023-05-26 12:08:21 +0100

    day3 changes

--- day3.py
+++ day3.py
@@ -1,2 +1,15 @@
-def mine():
-    dj
+def register_check(a):
+    in_school = a.values()
+    list_in_school = list(in_school)
+    num_in_school = list_in_school.count("yes")
+
+    return num_in_school
+
+
+register = {'Michael':'yes',
+            'John': 'no',
+            'Peter':'yes', 
+            'Mary': 'yes'
+        }
+
+print(register_check(register))
