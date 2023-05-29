4a4072bfa1fd3aed7ce6d773fdca2e338c98bf91
Odartei Lamptey <odarteilamptey.ml@gmail.com>
2023-05-29 17:33:52 +0100

    Day 6 solutions

---
+++ day6.py
@@ -0,0 +1,23 @@
+# def user_name():
+#     email_add = input("Please input your email address: ")
+#     splitted_email = email_add.split("@")
+#     sliced_email = splitted_email[ :1]
+
+#     for userName in sliced_email:
+#         return (f"Your user name is: {userName}")  
+    
+# print(user_name())
+
+#-----------------Second Question ----------------------------
+
+
+def zeroed(a):
+    if len(a) >= 2:
+        a[0] = 0
+        a[-1] = 0
+
+    return a
+
+sample_list = [1,2,3,4,5,6,7,8]
+
+print(zeroed(sample_list))
